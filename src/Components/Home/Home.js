292a7d160f41f4ae90144b99bb4f94b663cfe69f
sam <sam.nnodim@gmail.com>
2017-01-05 22:18:43 -0800

    added middle scroll section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -13,7 +13,7 @@ class Home extends Component {
     return (
       <div className="HomePage">
         {/** HEAD PAGE WRAPPUH **/}
-        <div className="Home black-bg vh-100 min-vh-100 pa3 bb b--white">
+        <div className="Home black-bg min-vh-100 min-vh-100 pa3">
           {/** U. FIRST SET **/}
             {/** --- nerd wave ~ loader --- **/}
 
@@ -30,7 +30,39 @@ class Home extends Component {
               </div>
             </nav>
 
-            {/** -- middle -- **/}
+            {/** -- middle (skroll sections) -- **/}
+            <article className="mw5 mw7-ns center bg-light-gray pa3 pa5-ns">
+            
+              <h4 className="f6">16:9</h4>
+              <div className="aspect-ratio aspect-ratio--16x9 mb4">
+                <div className="aspect-ratio--object cover " id="pic"></div>
+              </div>
+
+              <h4 className="f6">8:5</h4>
+              <div className="aspect-ratio aspect-ratio--8x5 mb4">
+                <div className="aspect-ratio--object cover" id="pic"></div>
+              </div>
+
+              <h4 className="f6">7:5</h4>
+              <div className="aspect-ratio aspect-ratio--7x5 mb4">
+                <div className="aspect-ratio--object cover" id="pic"></div>
+              </div>
+
+              <h4 className="f6">6:4</h4>
+              <div className="aspect-ratio aspect-ratio--6x4 mb4">
+                <div className="aspect-ratio--object cover" id="pic"></div>
+              </div>
+
+              <h4 className="f6">4:3</h4>
+              <div className="aspect-ratio aspect-ratio--4x3 mb4">
+                <div className="aspect-ratio--object cover" id="pic"></div>
+              </div>
+
+              <h4 className="f6">1:1</h4>
+              <div className="aspect-ratio aspect-ratio--1x1 mb4">
+                <div className="aspect-ratio--object cover" id="pic"></div>
+              </div>
+            </article>
 
 
             {/** -- background videos -- **/}
