23a2cea28a24fe8dbf64b19c3aea385d719ff010
sam <sam.nnodim@gmail.com>
2017-01-06 11:01:12 -0800

    changed something

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -23,16 +23,16 @@ class Home extends Component {
             <nav className="pa3 pa4-ns">
               <a className="link dim green hover-light-purple b f1 f-headline-ns tc db mb3 mb4-ns" href="#" title="Home">nerdwave</a>
               <div className="tc pb3">
-                <a className="link dim gray f6 f5-ns dib mr3" href="#" title="Home">Home</a>
-                <a className="link dim gray f6 f5-ns dib mr3" href="#" title="About">About</a>
-                <a className="link dim gray f6 f5-ns dib mr3" href="#" title="Store">Store</a>
-                <a className="link dim gray f6 f5-ns dib" href="#" title="Contact">Contact</a>
+                <a className="link dim red f6 f5-ns dib mr3" href="#" title="Home">Home</a>
+                <a className="link dim red f6 f5-ns dib mr3" href="#" title="About">About</a>
+                <a className="link dim red f6 f5-ns dib mr3" href="#" title="Store">Store</a>
+                <a className="link dim red f6 f5-ns dib" href="#" title="Contact">Contact</a>
               </div>
             </nav>
 
             {/** -- middle (skroll sections) -- **/}
             <article className="mw5 mw7-ns center bg-light-gray pa3 pa5-ns">
-            
+
               <h4 className="f6">16:9</h4>
               <div className="aspect-ratio aspect-ratio--16x9 mb4">
                 <div className="aspect-ratio--object cover " id="pic"></div>
